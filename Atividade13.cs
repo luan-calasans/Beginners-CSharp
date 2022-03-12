8d30d2b4b9b8be77b14d60cf4768307bdace530a
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-03-12 14:33:36 -0300

    pdc

--- Atividade13.cs
+++ Atividade13.cs
@@ -1,85 +1,29 @@
 ﻿using System;
 
-namespace PDC_ULTIMO_EXERCÍCIO
+namespace ALTURA
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int P, M, G;
-            Console.Write("Digite o primeiro número: ");
+            Console.Write("Digite sua altura (centímetros): ");
             int A = int.Parse(Console.ReadLine());
-            Console.Write("Digite o segundo número: ");
-            int B = int.Parse(Console.ReadLine());
-            Console.Write("Digite o terceiro número: ");
-            int C = int.Parse(Console.ReadLine());
-            if (A > B) // A > B 
+
+            if (A <= 160)
             {
-                if (B < C) // A > B    ->    B < C  
+                Console.WriteLine("Você baixo(a)");
+            }
+            else
+            {
+                if (A >= 170 && A <= 175)
                 {
-                    if (A > C) // A > B    ->    C > B    ->    A > C    
-                    {
-                        G = A;
-                        P = B;
-                        M = C;
-                    }
-                    else // A > B    ->    C > B    ->    C > A
-                    { 
-                        G = C; 
-                        M = A;
-                        P = B;
-                    }
+                    Console.WriteLine("Sua altura é mediana");
                 }
-                else // A > B    ->    B > C
+                else
                 {
-                    if (A > C) // A > B    ->    B > C    ->    A > C   
-                    {
-                        G = A;
-                        M = B;
-                        P = C;
-                    }
-                    else // A > B    ->    B > C    ->    C > A
-                    {
-                        G = C;
-                        M = A;
-                        P = B;
-                    }
+                    Console.WriteLine("Você é alto(a)");
                 }
             }
-            else // B > A   
-            {
-                if (A > C) // B > A    ->    A > C         
-                {
-                    if (B > C) // B > A    ->    A > C    ->    B > C 
-                    {
-                        G = B;
-                        M = A;
-                        P = C;
-                    }
-                    else // B > A    ->    C > A    ->    C > B 
-                    { 
-                        G = C;
-                        M = B;
-                        P = A;
-                    }
-                }
-                else // B > A    ->    C > A
-                {
-                    if (C > B) // B > A    ->    C > A    ->    C > B 
-                    {
-                        G = C;
-                        P = A;
-                        M = B;
-                    }
-                    else // B > A    ->    C > A    ->    B > C 
-                    { 
-                        G = C;
-                        P = A;
-                        M = B;
-                    }
-                }
-            }
-            Console.WriteLine($"A ordem descrescente é: {G} > {M} > {P}");
             Console.ReadKey();
         }
     }
