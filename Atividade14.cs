28bf2031d9259af9bd6fd8d06de539782d34f8d4
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-03-22 14:56:10 -0300

    pdc

---
+++ Atividade14.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite sua altura: ");
+            double altura = double.Parse(Console.ReadLine());
+            if (altura < 1.76)
+            {
+                if (altura < 1.60)
+                {
+                    Console.Write("Você tem estatura baixa.");
+                }
+                else
+                {
+                    Console.Write("Você tem estatura mediana.");
+                }
+            }
+            else
+            {
+                if (altura < 2)
+                {
+                    Console.Write("Você tem estatura alta.");
+                }
+                else
+                {
+                    Console.Write("Você tem estatura muito alta.");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
