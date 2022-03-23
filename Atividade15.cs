c2918ba2c27302d94cdcc686787c7687fe5c8162
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-03-23 10:56:39 -0300

    pdc

---
+++ Atividade15.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDADE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite sua idade: ");
+            int idade = int.Parse(Console.ReadLine()); 
+
+            if (idade >= 0 && idade <= 11)
+            {
+                Console.WriteLine("Você é uma criança");
+            }
+            else if (idade >= 12 && idade <= 17)
+            {
+                Console.WriteLine("Você é adolescente");
+            }
+            else if (idade >= 18 && idade <= 60)
+            {
+                Console.WriteLine("Você é adulto");
+            }
+            else
+            {
+                Console.WriteLine("Você é idoso(a)");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
