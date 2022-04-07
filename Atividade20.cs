086bb3adab80e70268741ec8201ccc8b2757f2e8
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-04-07 00:25:16 -0300

    Update Atividade20.cs

--- Atividade20.cs
+++ Atividade20.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace prj37461_lista03_ex04
+namespace atividades
 {
-    internal class C37461_lista03_ex04
+    internal class ex20
     {
         static void Main(string[] args)
         {
