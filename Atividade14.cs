231dc36d1e391e7d76a526fa0e28a5857fc3e634
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-04-07 00:28:42 -0300

    Update Atividade14.cs

--- Atividade14.cs
+++ Atividade14.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Altura
+namespace atividades
 {
-    class Program
+    class ex14
     {
         static void Main(string[] args)
         {
