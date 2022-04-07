3d16ce52b15851d6a988c451d2280630fd1f548e
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-04-07 00:27:53 -0300

    Update Atividade17.cs

--- Atividade17.cs
+++ Atividade17.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace prj37461_lista03_ex01
+namespace atividades
 {
-    internal class C37461_lista03_ex01
+    internal class ex17
     {
         static void Main(string[] args)
         {
