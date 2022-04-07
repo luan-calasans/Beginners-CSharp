20a6cc8efab9081837b34c3e153b416cd695f91a
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-04-07 00:24:39 -0300

    Update Atividade21.cs

--- Atividade21.cs
+++ Atividade21.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace prj37461_lista03_ex05
+namespace atividades
 {
-    internal class C37461_lista03_ex05
+    internal class ex21
     {
         static void Main(string[] args)
         {
