6878d1ac5ba1bff48a8b329f07ba004497b8b7e9
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-04-27 22:13:09 -0300

    Update Atividade27.cs

--- Atividade27.cs
+++ Atividade27.cs
@@ -14,13 +14,13 @@ namespace Atividades
              Desenvolva um programa que receba 3 números via teclado e no final exiba o maior.
              Ps: usar somente duas variáveis
              */
-            int firstNumber = 0, secondNumber = 0;
+            double firstNumber = 0, secondNumber = 0;
             try
             {
                 Console.Write("Digite o primeiro número: ");
-                firstNumber = int.Parse(Console.ReadLine());
+                firstNumber = double.Parse(Console.ReadLine());
                 Console.Write("Digite o segundo número: ");
-                secondNumber = int.Parse(Console.ReadLine());
+                secondNumber = double.Parse(Console.ReadLine());
             }
             catch
             {
@@ -31,12 +31,12 @@ namespace Atividades
             if (firstNumber > secondNumber)
             {
                 Console.Write("Digite o terceiro número: ");
-                secondNumber = int.Parse(Console.ReadLine());
+                secondNumber = double.Parse(Console.ReadLine());
             }
             else
             {
                 Console.Write("Digite o terceiro número: ");
-                firstNumber = int.Parse(Console.ReadLine());
+                firstNumber = double.Parse(Console.ReadLine());
             }
             if (firstNumber > secondNumber)
             {
