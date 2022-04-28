c73bccad12ea2ca948f269d179dbf2b9ca883fe8
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-04-27 22:52:02 -0300

    Add files via upload

---
+++ Atividade29.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividades
+{
+    internal class ex29
+    {
+        static void Main(string[] args)
+        {
+            /*
+               Desenvolva um programa que receba três valores via teclado e no final exiba a soma. 
+               Ps: utilizando somente duas variáveis
+             */
+
+            double userData = 0, sum = 0;
+            try
+            {
+                Console.Write("Digite um número: ");
+                userData = double.Parse(Console.ReadLine());
+                sum += userData;
+
+                Console.Write("Digite um número: ");
+                userData = double.Parse(Console.ReadLine());
+                sum += userData;
+
+                Console.Write("Digite um número: ");
+                userData = double.Parse(Console.ReadLine());
+                sum += userData;
+            }
+            catch
+            {
+                Console.WriteLine("Digite apenas números!");
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine($"A soma foi: {sum}");
+            Console.ReadKey();
+        }
+    }
+}
