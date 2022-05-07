f6906e561f071d8c649d31b9f12d5a7fe6ad3e53
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-05-07 20:45:09 -0300

    Update Atividade36.cs

--- Atividade36.cs
+++ Atividade36.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace prj37461_lista03_ex02
+namespace Atividades
 {
-    internal class C37461_lista03_ex02
+    internal class ex36
     {
         static void Main(string[] args)
         {
