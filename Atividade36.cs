e1a5e2a2a939c8704c879a882b0878df41adcb07
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-05-07 20:44:44 -0300

    Add files via upload

---
+++ Atividade36.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj37461_lista03_ex02
+{
+    internal class C37461_lista03_ex02
+    {
+        static void Main(string[] args)
+        {
+            // Entrar via teclado com o sexo de determinado usuário, aceitar somente “F” ou “M” como respostas válidas.
+            string gender;
+            try
+            {
+                Console.Write("'F' para feminino 'M' para masculino\nDigite seu gênero: ");
+                gender = Console.ReadLine();
+                do
+                {
+                    Console.WriteLine("Digite apenas 'F' ou 'M'");
+                    Console.Write("Digite seu gênero: ");
+                    gender = Console.ReadLine();
+                } while (gender != "F" && gender != "M");
+
+                if (gender == "M")
+                {
+                    Console.Write("Seu gênero é Masculino!");
+                }
+                else 
+                {
+                    Console.Write("Seu gênero é Feminino!");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Digite apenas 'F' ou 'M'");
+                return;
+            }
+            Console.ReadKey();
+        }
+    }
+}
