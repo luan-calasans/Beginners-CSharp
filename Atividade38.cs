2d83581ed9063e752ac171dd1e9a45102a18ff78
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-05-08 11:48:50 -0300

    Add files via upload

---
+++ Atividade38.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj37461_lista03_ex03
+{
+    internal class C37461_lista03_ex03
+    {
+        static void Main(string[] args)
+        {
+            // Exibir a soma dos números positivos do intervalo de um a cem.
+            int value, sum = 0;
+            try
+            {
+                do
+                {
+                    Console.Write("Digite um número positivo (entre 1 e 100): ");
+                    value = int.Parse(Console.ReadLine());
+                    sum += value;
+                } while (sum > 0 && sum < 100);
+            }
+            catch
+            {
+                Console.WriteLine("Digite apenas números positivos!");
+            }
+            Console.WriteLine($"Soma = {sum}");
+            Console.ReadKey();
+        }
+    }
+}
