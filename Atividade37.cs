091c2009b888a5e80feda246e3e85eeac6172a5a
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-05-08 10:41:36 -0300

    Add files via upload

---
+++ Atividade37.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj37461_lista03_ex03
+{
+    internal class C37461_lista03_ex03
+    {
+        static void Main(string[] args)
+        {
+            // Entrar via teclado com dez valores positivos. Consistir a digitação e enviar mensagem de erro, se necessário. Após a digitação, exibir:
+            // a.O maior valor;
+            // b.A soma dos valores;
+            // c.A média aritmética dos valores.
+            double count = 0, value, highestValue = 0, sum = 0, average = 0;
+            try
+            {
+                while (count < 10)
+                {
+                    Console.Write("Digite apenas números positivos: ");
+                    value = double.Parse(Console.ReadLine());
+                    if (value < 0)
+                    {
+                        Console.Write("ERRO!\nDigite apenas números positivos: ");
+                        value = double.Parse(Console.ReadLine());
+                        if (value > 0)
+                        {
+                            count++;
+                            sum += value;
+                            average = sum / count;
+                            if (value > highestValue)
+                            {
+                                highestValue = value;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("ERRO!");
+                        }
+                    }
+                    else
+                    {
+                        count++;
+                        sum += value;
+                        average = sum / count;
+                        if (value > highestValue)
+                        {
+                            highestValue = value;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Digite apenas números positivos!");
+                return;
+            }
+            Console.Write($"Maior valor = {highestValue}\nSoma dos valores = {sum}\nMédia dos valores = {average}");
+            Console.ReadKey();
+        }
+    }
+}
