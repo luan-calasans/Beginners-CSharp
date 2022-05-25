0f5c1203f6900a205614b94d59ec32256add5362
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-05-25 17:20:57 -0300

    Add files via upload

---
+++ Atividade43.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividades
+{
+    internal class ex43
+    {
+        static void Main(string[] args)
+        {
+            // Desenvolva um programa que exiba a tabuada de acordo com o número que o usuário pedir
+            double multiplier = 0, userData;
+            Console.WriteLine("===============");
+            Console.WriteLine("    TABUADA    ");
+            Console.WriteLine("===============");
+            Console.Write("Informe um número a ser multiplicado: ");
+            userData = double.Parse(Console.ReadLine());
+            for (int control = 0; control < 11; control++)
+            {
+                Console.WriteLine($" {userData} x {control} = {multiplier * userData}");
+                multiplier++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
