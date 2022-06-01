afc5c7f6fe6bd18a44b5b2e67b4dbda1b961c262
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-06-01 16:26:07 -0300

    Add files via upload

---
+++ Atividade45.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividades
+{
+    internal class ex45
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Receba 10 números via teclado e exiba:
+                - Maior
+                - Soma
+                - Média 
+             */
+            double userData, bigger = 0, sum = 0, average;
+            try
+            {
+                for (int controle = 0; controle < 10; controle++)
+                {
+                    Console.Write("Digite um número:");
+                    userData = double.Parse(Console.ReadLine());
+                    if (userData > bigger)
+                    {
+                        bigger = userData;
+                    }
+                    sum += userData;
+                }
+                average = sum / 10;
+            }
+            catch
+            {
+                Console.WriteLine("Digite apenas números!");
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine($"Maior: {bigger}\nSoma: {sum}\nMédia: {average}");
+            Console.ReadKey();
+        }
+    }
+}
