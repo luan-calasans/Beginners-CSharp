6606d508fa114808a9cacedaf0b520104312bf18
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-06-13 17:04:36 -0300

    Add files via upload

---
+++ Atividade54.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj37461_ListaVetores_ex05
+{
+    internal class C37461_ListaVetores_ex05
+    {
+        static void Main(string[] args)
+        {
+            /*             
+                Criar um vetor A com 10 elementos inteiros. Escrever um programa que calcule e escreva: 
+                a) a soma de elementos armazenados neste vetor que são inferiores a 15; 
+                b) a quantidade de elementos armazenados no vetor que são iguais a 15; 
+                c) a média dos elementos armazenados no vetor que são superiores a 15.
+            */
+            int[] numeroInteiro = new int[10];
+            int quantidadeIgual15 = 0, somaInferior15 = 0;
+            double mediaSuperior15, quantidadeSuperior15 = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Digite um número inteiro positivo: ");
+                numeroInteiro[i] = int.Parse(Console.ReadLine());
+                while(numeroInteiro[i] < 1)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red; Console.Write("ERRO! ");
+                    Console.ForegroundColor = ConsoleColor.Gray; Console.Write("Digite um número inteiro positivo: ");
+                    numeroInteiro[i] = int.Parse(Console.ReadLine());
+                }
+                if (numeroInteiro[i] < 15)
+                {
+                    somaInferior15 += numeroInteiro[i];
+                }
+                if (numeroInteiro[i] == 15)
+                {
+                    quantidadeIgual15++;
+                }
+                if (numeroInteiro[i] > 15)
+                {
+                    quantidadeSuperior15++;
+                }
+            }
+            mediaSuperior15 = quantidadeSuperior15 / numeroInteiro.Length;
+            Console.ForegroundColor = ConsoleColor.DarkCyan; Console.Write($"Soma dos elementos inferiores a 15: {somaInferior15}\nQuantidade de elementos iguais a 15: {quantidadeIgual15}\nMédia dos elementos superiores a 15: {mediaSuperior15}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Atividade55.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj37461_ListaVetores_ex06
+{
+    internal class C37461_ListaVetores_ex06
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Ler as duas notas bimestrais para um conjunto de 10 alunos. Armazenar as notas informadas 
+                em dois vetores “Nota1” e “Nota2” do tipo real. Escreva um programa que calcule a média
+                aritmética simples das notas informadas armazenando o resultado em um vetor “Result” de 
+                mesmo tipo e tamanho. Ao mostrar os resultados exibir a situação de cada aluno. Se a
+                média calculada for superior ou igual a 7 o aluno estará “aprovado”, caso contrário, 
+                a situação do aluno será “reprovado”.
+             */
+            string[] aluno = new string[10];
+            double[] nota1 = new double[10], nota2 = new double[10], result = new double[10];
+            Console.WriteLine("================================");
+            Console.WriteLine("  MÉDIA BIMESTRAL DE 10 ALUNOS  ");
+            Console.WriteLine("================================");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Digite seu nome: ");
+                aluno[i] = Console.ReadLine();
+
+                Console.Write("Digite sua 1º nota: ");
+                nota1[i] = double.Parse(Console.ReadLine());
+
+                Console.Write("Digite sua 2º nota: ");
+                nota2[i] = double.Parse(Console.ReadLine());
+
+                result[i] = (nota1[i] + nota2[i]) / 2;
+                if (result[i] >= 7)
+                {
+                    Console.Write($"Parabéns {aluno[i]}! Você foi ");
+                    Console.ForegroundColor = ConsoleColor.Green; Console.Write("aprovado(a)"); 
+                    Console.ForegroundColor = ConsoleColor.Gray; Console.Write($" com média {result[i]}\n");
+                }
+                else
+                {
+                    Console.Write($"Sinto muito {aluno[i]}! Você foi ");
+                     Console.ForegroundColor = ConsoleColor.Red;Console.Write("reprovado(a)");
+                    Console.ForegroundColor = ConsoleColor.Gray; Console.Write($" com média {result[i]}\n");
+                }
+                if (i != 9)
+                {
+                    Console.WriteLine("-------------------------------------------------");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
