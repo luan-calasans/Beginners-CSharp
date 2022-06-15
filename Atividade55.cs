39ec71edfe9b0eb293c3a07fe28dcb17983aef11
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-06-15 20:51:03 -0300

    Add files via upload

--- Atividade55.cs
+++ Atividade55.cs
@@ -4,55 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Atividades
+namespace prj37461_ListaVetores_ex07
 {
-    internal class ex55
+    internal class C37461_ListaVetores_ex07
     {
         static void Main(string[] args)
         {
-            /*
-                Ler as duas notas bimestrais para um conjunto de 10 alunos. Armazenar as notas informadas 
-                em dois vetores “Nota1” e “Nota2” do tipo real. Escreva um programa que calcule a média
-                aritmética simples das notas informadas armazenando o resultado em um vetor “Result” de 
-                mesmo tipo e tamanho. Ao mostrar os resultados exibir a situação de cada aluno. Se a
-                média calculada for superior ou igual a 7 o aluno estará “aprovado”, caso contrário, 
-                a situação do aluno será “reprovado”.
-             */
-            string[] student = new string[10];
-            double[] grade1 = new double[10], grade2 = new double[10], result = new double[10];
-            Console.WriteLine("================================");
-            Console.WriteLine("  MÉDIA BIMESTRAL DE 10 ALUNOS  ");
-            Console.WriteLine("================================");
+            /* Ler 10 elementos inteiros em um vetor A. Montar uma rotina de busca, 
+               para pesquisar se um dado elemento X esta armazenado em A. */
+            int contador = 1, numeroPesquisado;
+            int[] numeroInteiro = new int[10];
+            int[] numeroDigitado = new int[10];
+
             for (int i = 0; i < 10; i++)
             {
-                Console.Write("Digite seu nome: ");
-                student[i] = Console.ReadLine();
-
-                Console.Write("Digite sua 1º nota: ");
-                grade1[i] = double.Parse(Console.ReadLine());
-
-                Console.Write("Digite sua 2º nota: ");
-                grade2[i] = double.Parse(Console.ReadLine());
-
-                result[i] = (nota1[i] + nota2[i]) / 2;
-                if (result[i] >= 7)
-                {
-                    Console.Write($"Parabéns {student[i]}! Você foi ");
-                    Console.ForegroundColor = ConsoleColor.Green; Console.Write("aprovado(a)"); 
-                    Console.ForegroundColor = ConsoleColor.Gray; Console.Write($" com média {result[i]}\n");
-                }
-                else
-                {
-                    Console.Write($"Sinto muito {student[i]}! Você foi ");
-                     Console.ForegroundColor = ConsoleColor.Red;Console.Write("reprovado(a)");
-                    Console.ForegroundColor = ConsoleColor.Gray; Console.Write($" com média {result[i]}\n");
-                }
-                if (i != 9)
-                {
-                    Console.WriteLine("-------------------------------------------------");
-                }
+                Console.Write($"{contador}º número: " );
+                numeroDigitado[i] = int.Parse(Console.ReadLine());
+                contador++;
             }
+
+            Console.Write("Digite um número a ser pesquisado: ");
+            numeroPesquisado = int.Parse(Console.ReadLine());
+            
+            if (numeroDigitado[9] == numeroPesquisado || numeroDigitado[8] == numeroPesquisado || numeroDigitado[7] == numeroPesquisado || numeroDigitado[6] == numeroPesquisado || numeroDigitado[5] == numeroPesquisado || numeroDigitado[4] == numeroPesquisado || numeroDigitado[3] == numeroPesquisado || numeroDigitado[2] == numeroPesquisado || numeroDigitado[1] == numeroPesquisado || numeroDigitado[0] == numeroPesquisado)
+            {
+                Console.Write("O número");
+                Console.ForegroundColor = ConsoleColor.Green; Console.Write(" está ");
+                Console.ForegroundColor = ConsoleColor.Gray; Console.Write("armazenado no vetor");
+            }
+            else
+            {
+                Console.Write("O número");
+                Console.ForegroundColor = ConsoleColor.Red; Console.Write(" não está ");
+                Console.ForegroundColor = ConsoleColor.Gray; Console.Write("armazenado no vetor");
+            }
+            
             Console.ReadKey();
+
         }
     }
 }
---
+++ Atividade56.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prj37461_ListaVetores_ex08
+{
+    internal class C37461_ListaVetores_ex08
+    {
+        static void Main(string[] args)
+        {
+            // Ler 10 elementos inteiros positivos em um vetor A e colocá-los em
+            // ordem crescente e apresentar os elementos ordenados.
+
+            int[] numeroInteiro = new int[10];
+            int auxiliar, y;
+
+            for (int x = 0; x < 10; x++)
+            {
+                Console.Write("Digite um número inteiro: ");
+                numeroInteiro[x] = int.Parse(Console.ReadLine());
+                for (y = 0;  y < 9; y++)
+                {
+                    if (numeroInteiro[x] < numeroInteiro[y])
+                    {
+                        auxiliar = numeroInteiro[x];
+                        numeroInteiro[x] = numeroInteiro[y];
+                        numeroInteiro[y] = auxiliar;
+                    }
+                }
+            }
+            Console.Write("Ordem crescente dos número digitados: ");
+            for (int z = 0; z < 10; z++)
+            {
+                Console.Write($" {numeroInteiro[z]} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
