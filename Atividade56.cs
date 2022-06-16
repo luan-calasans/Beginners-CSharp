2a3b60527bf0bc74d05eab547ccbf8f7b6bfa033
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-06-15 21:07:58 -0300

    Update Atividade56.cs

--- Atividade56.cs
+++ Atividade56.cs
@@ -4,36 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace prj37461_ListaVetores_ex08
+namespace Atividades
 {
-    internal class C37461_ListaVetores_ex08
+    internal class ex56
     {
         static void Main(string[] args)
         {
             // Ler 10 elementos inteiros positivos em um vetor A e colocá-los em
             // ordem crescente e apresentar os elementos ordenados.
 
-            int[] numeroInteiro = new int[10];
-            int auxiliar, y;
+            int[] number = new int[10];
+            int aux, y;
 
             for (int x = 0; x < 10; x++)
             {
                 Console.Write("Digite um número inteiro: ");
-                numeroInteiro[x] = int.Parse(Console.ReadLine());
+                number[x] = int.Parse(Console.ReadLine());
                 for (y = 0;  y < 9; y++)
                 {
-                    if (numeroInteiro[x] < numeroInteiro[y])
+                    if (number[x] < number[y])
                     {
-                        auxiliar = numeroInteiro[x];
-                        numeroInteiro[x] = numeroInteiro[y];
-                        numeroInteiro[y] = auxiliar;
+                        aux = number[x];
+                        number[x] = number[y];
+                        number[y] = aux;
                     }
                 }
             }
             Console.Write("Ordem crescente dos número digitados: ");
             for (int z = 0; z < 10; z++)
             {
-                Console.Write($" {numeroInteiro[z]} ");
+                Console.Write($" {number[z]} ");
             }
             Console.ReadKey();
         }
