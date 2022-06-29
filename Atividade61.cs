655901f6c0f5cab0efa7de3a0a6f482d884f05f6
Luan Calasans <69552520+luan-calasans@users.noreply.github.com>
2022-06-29 16:36:20 -0300

    Add files via upload

---
+++ Atividade61.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Atividades
+{
+    class ex61
+    {
+        static void Main(string[] args)
+        {
+            int[] vector = new int[5];
+            int i,j,aux;
+
+            for (i = 0; i < vector.Length; i++)
+            {
+                Console.Write("{0}º elemento : ", i + 1);
+                vector[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (i = 0; i < vector.Length - 1; i++)
+            {
+                for (j = i + 1; j < vector.Length; j++)
+                {
+                    if (vector[i] > vector[j])
+                    {  
+                        aux = exchange(vector, i, j);  
+                    }
+                }
+            }
+
+            Console.Clear();
+
+            for (i = 0; i < vector.Length; i++)
+            {
+                Console.WriteLine("{0}º elemento : {1}", i + 1, vector[i]);
+            }
+
+            Console.ReadKey();
+        }
+        private static int exchange(int[] vector, int i, int j)
+        {
+            int aux = vector[i];
+            vector[i] = vector[j];
+            vector[j] = aux;
+            return aux;
+        }
+    }
+}
